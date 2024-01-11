b5e17a39101fd93b38dfa2a46206309fd1b2a0f6
marleypm16 <[200~rmcctec@proton.me>
2024-01-11 15:52:49 -0300

    feat: continue,countdown,limparInputs e mais constantes

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const btnIniciar = document.querySelector(".iniciar")
 const tempo = document.querySelector('.timer')
-const hora = document.querySelector('.hour')
-const minuto = document.querySelector('.minute')
-const segundos = document.querySelector('.second')
+const horaInput = document.querySelector('.hour')
+const minutoInput = document.querySelector('.minute')
+const segundosInput = document.querySelector('.second')
+const horaSpan = document.querySelector('.span-hora')
+const minutoSpan = document.querySelector('.span-minutos')
+const segundosSpan = document.querySelector('.span-segundos')
 const btnStop = document.querySelector(".parar")
 const btnReiniciar = document.querySelector(".reiniciar")
+const btnContinuar = document.querySelector(".continuar")
 
 class Timer{
     constructor(){
@@ -22,10 +26,40 @@ class Timer{
         return isNaN(valor)
     }
 
+    limparInputs(){
+        horaInput.value = ''
+        minutoInput.value = ''
+        segundosInput.value = ''
+    }
+    countdown(hour,minutes,seconds){
+        this.intervalId = setInterval(() => {
+            if(minutes == 0 && seconds == 0 && hour > 0){
+                hour--
+                minutes = 60
+            }
+            // if(this.minutes == 0 && this.seconds == 0){
+            //     this.minutes = 60
+            // }
+            if(seconds == 0 && minutes > 0){
+                minutes--
+                seconds = 60
+            }
+            seconds--
+            // tempo.innerHTML = `${this.formatarNumeros(this.hour)}:${this.formatarNumeros(this.minutes)}:${this.formatarNumeros(this.seconds)}`;
+            horaSpan.innerHTML = `${this.formatarNumeros(hour)}`
+            minutoSpan.innerHTML = `${this.formatarNumeros(minutes)}`
+            segundosSpan.innerHTML = `${this.formatarNumeros(seconds)}`
+            if (hour == 0 && minutes == 0 && seconds == 0) {
+                clearInterval(this.intervalId)
+                return alert("fim")
+             }
+        }, 1000);
+    }
+
     iniciar() {
-        this.hour = parseInt(hora.value,10)
-        this.minutes = parseInt(minuto.value,10)
-        this.seconds = parseInt(segundos.value,10)
+        this.hour = parseInt(horaInput.value,10)
+        this.minutes = parseInt(minutoInput.value,10)
+        this.seconds = parseInt(segundosInput.value,10)
         if(this.verificarSeNumero(this.hour)){
             this.hour = 0
         }
@@ -38,39 +72,32 @@ class Timer{
         if (this.hour == 0 && this.minutes == 0 && this.seconds == 0) {
            return alert("insira um tempo")
         }
-        this.intervalId = setInterval(() => {
-         
-
-            if(this.minutes == 0 && this.seconds == 0 && this.hour > 0){
-                this.hour--
-                this.minutes = 60
-            }
-            // if(this.minutes == 0 && this.seconds == 0){
-            //     this.minutes = 60
-            // }
-            if(this.seconds == 0 && this.minutes > 0){
-                this.minutes--
-                this.seconds = 60
-            }
-            this.seconds--
-            tempo.innerHTML = `${this.formatarNumeros(this.hour)}:${this.formatarNumeros(this.minutes)}:${this.formatarNumeros(this.seconds)}`;
-            console.log(`Time remaining: ${this.time}s`);
-            if (this.hour == 0 && this.minutes == 0 && this.seconds == 0) {
-                clearInterval(intervalId)
-                return alert("fim")
-             }
-        }, 1000);
+        this.countdown(this.hour,this.minutes,this.seconds)
+        this.limparInputs()
+       
     }
     parar(){
         clearInterval(this.intervalId)
         console.log('parou')
+    }
+    continue(){
+        const hour = parseInt(horaSpan.innerHTML,10)
+        const minutes = parseInt(minutoSpan.innerHTML,10)
+        const seconds = parseInt(segundosSpan.innerHTML,10)
+        console.log(hour)
+        console.log(minutes)
+        console.log(seconds)
+        this.countdown(hour,minutes,seconds)
+
     }
     reiniciar() {
         clearInterval(this.intervalId);
         this.hour = 0;
         this.minutes = 0;
         this.seconds = 0;
-        tempo.innerHTML = `${this.formatarNumeros(this.hour)}:${this.formatarNumeros(this.minutes)}:${this.formatarNumeros(this.seconds)}`;
+        horaSpan.innerHTML = `${this.formatarNumeros(this.hour)}`
+        minutoSpan.innerHTML = `${this.formatarNumeros(this.minutes)}`
+        segundosSpan.innerHTML = `${this.formatarNumeros(this.seconds)}`
     }
 }
 
@@ -87,6 +114,10 @@ btnStop.addEventListener("click",()=>{
 
 btnReiniciar.addEventListener('click',()=>{
     timer.reiniciar()
+})
+
+btnContinuar.addEventListener("click",()=>{
+    timer.continue()
 })
 
 
